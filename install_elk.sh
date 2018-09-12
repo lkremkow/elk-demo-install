a7012f6b28403db231195f8c72a2339a85797787
Leif Kremkow <kremkow@tftg.net>
2018-09-12 17:36:58 +0200

    remove no longer needed file

--- install_elk.sh
+++ install_elk.sh
@@ -12,6 +12,8 @@ curl -O https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
 apt-key add GPG-KEY-elasticsearch
 
+rm GPG-KEY-elasticsearch
+
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" > /etc/apt/sources.list.d/elastic-6.x.list
 
 apt-get update
